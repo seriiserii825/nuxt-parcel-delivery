7306383c3bdd1378c2e30dcdb3e32367359dbbb2
serii burduja <seriiburduja@gmail.com>
2025-05-13 17:26:46 +0300

    feat: default layout

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useHead } from "#imports";
+
+useHead({
+  link: [
+    { rel: "stylesheet", href: "/css/fontawesome.min.css" },
+  ],
+});
+
+</script>
+
+<template>
+  <div class="layout default-layout">
+    <slot />
+  </div>
+</template>
+<style>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt rem unde sequi odit
+      blanditiis nam accusantium soluta fugit cupiditate. Nihil, provident tempora sunt dolores
+      nesciunt natus sit maiores assumenda eaque.
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+.home {
+  h1 {
+    color: red;
+  }
+}
+</style>
