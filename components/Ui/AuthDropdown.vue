6edae6721bbf2be38251bc7b83a3282f373c8349
serii burduja <seriiburduja@gmail.com>
2025-05-13 21:08:35 +0300

    feat: finished home page

---
+++ components/Ui/AuthDropdown.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+// import NavLink from "../NavLink.vue";
+// import useSweetAlertConfirm from "../../Composables/useSweetAlertConfirm";
+const show = ref(false);
+
+async function logOut() {
+  // const confirm = await useSweetAlertConfirm('Are you sure you want to log out?', 'You will be logged out of your account.');
+  // if (!confirm) {
+  //     return;
+  // }
+  // window.location.href = "/logout";
+}
+</script>
+
+<template>
+  <div class="relative">
+    <div @click="show = !show" class="flex gap-2 items-center cursor-pointer">
+      <span>myemail@gmail.com</span>
+      <i class="fa-solid fa-chevron-down"></i>
+    </div>
+    <div
+      v-show="show"
+      class="absolute top-[50px] right-0 bg-primary dark:bg-dark-primary p-2 rounded-md flex flex-col gap-2 transition"
+    >
+      <NuxtLink class="flex gap-2 items-center" to="/">
+        <span>Dashboard</span>
+      </NuxtLink>
+      <NuxtLink to="/">Logout</NuxtLink>
+    </div>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 import { useHead } from "#imports";
 import { switchTheme } from "~/utils/theme";
 useHead({
-  link: [{ rel: "stylesheet", href: "/css/fontawesome.min.css" }],
+  link: [{ rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" }],
 });
 
 
@@ -19,6 +19,7 @@ setThemeOnLoad();
       <nav class="p-6 mx-auto max-w-screen-lg flex items-center justify-between gap-2">
         <Logo />
         <div class="flex items-center space-x-6 gap-2">
+          <UiAuthDropdown/>
           <button
             @click="changeTheme"
             class="w-6 h-6 grid place-items-center rounded-full hover:outline outline-1 outline-white"
@@ -28,7 +29,7 @@ setThemeOnLoad();
         </div>
       </nav>
     </header>
-    <main class="text-color bg-primary-light dark:bg-dark-primary-dark dark:text-dark-color min-h-[100vh]">
+    <main class="text-color bg-primary-light dark:bg-dark-primary-dark dark:text-dark-color min-h-[100vh] p-6">
       <slot />
     </main>
   </div>
