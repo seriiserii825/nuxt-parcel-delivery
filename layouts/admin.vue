6117c5ed479c88015baf1ef96072e92e230a3520
serii burduja <seriiburduja@gmail.com>
2025-05-14 21:16:10 +0300

    feat: finished admin layout

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,26 +1,32 @@
 <script setup lang="ts">
 import { useHead } from "#imports";
-import {useUserStore} from "~/store/useUserStore";
+import { useUserStore } from "~/store/useUserStore";
 import { switchTheme } from "~/utils/theme";
 useHead({
-  link: [{ rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" }],
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css",
+    },
+  ],
 });
-
 const user_store = useUserStore();
-
 function changeTheme() {
   switchTheme();
 }
-
 setThemeOnLoad();
 </script>
-
 <template>
+  <div class="bg-gray-50 dark:bg-dark-primary dark:text-dark-color">
+  <AdminHeader />
   <div class="flex">
     <AdminSidebar />
-    <main class="flex-1 h-screen bg-gray-50 dark:bg-gray-700 min-h-screen">
-      <slot />
+    <main class="flex-1 h-screen min-h-screen">
+      <div class="p-4">
+        <slot />
+      </div>
     </main>
+  </div>
   </div>
 </template>
 <style>
