052b1f3ac20060876869c0698429ba6128aecb75
serii burduja <seriiburduja@gmail.com>
2025-05-14 10:10:37 +0300

    feat: logo nuxt-link

--- components/Logo.vue
+++ components/Logo.vue
@@ -3,8 +3,8 @@ const logo_url = "/images/logo.svg";
 </script>
 
 <template>
-    <a href="/" class="flex item-end gap-2">
-        <img :src="logo_url" alt="Project Manager Icon" />
-        <span class="line-clamp-none dark:text-dark-color relative top-0.5" >Parcel Delivery</span>
-    </a>
+  <NuxtLink href="/" class="flex item-end gap-2">
+    <img :src="logo_url" alt="Project Manager Icon" />
+    <span class="line-clamp-none dark:text-dark-color relative top-0.5">Parcel Delivery</span>
+  </NuxtLink>
 </template>
