e319631da60958d236d16edb49ab839314158263
serii burduja <seriiburduja@gmail.com>
2025-05-15 12:48:59 +0300

    feat: navheader

---
+++ components/Nav/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {useUserStore} from '~/store/useUserStore';
+
+const user_store = useUserStore();
+</script>
+
+<template>
+  <header>
+    <nav class="p-6 mx-auto max-w-screen-lg flex items-center justify-between gap-2">
+      <Logo />
+      <div class="flex items-center space-x-6 gap-2">
+        <UiAuthDropdown v-if="user_store.user" />
+        <UiThemeToggler />
+      </div>
+    </nav>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { useHead } from "#imports";
-import { useUserStore } from "~/store/useUserStore";
 useHead({
   link: [
     {
@@ -9,31 +8,18 @@ useHead({
     },
   ],
 });
-
-const user_store = useUserStore();
 </script>
 
 <template>
-  <div>
-    <header class="bg-primary text-color dark:bg-dark-primary dark:text-dark-color">
-      <nav class="p-6 mx-auto max-w-screen-lg flex items-center justify-between gap-2">
-        <Logo />
-        <div class="flex items-center space-x-6 gap-2">
-          <UiAuthDropdown v-if="user_store.user" />
-          <div v-else class="flex gap-2">
-            <FormBtn url="/login">Login</FormBtn>
-            <FormBtn url="/register">Register</FormBtn>
-          </div>
-          <UiThemeToggler />
-        </div>
-      </nav>
-    </header>
-    <main
-      class="text-color bg-primary-light dark:bg-dark-primary-dark dark:text-dark-color min-h-[100vh] p-6">
-      <div class="container m-auto">
+  <div
+    class="text-color bg-primary-light dark:bg-dark-primary-dark dark:text-dark-color min-h-[100vh]">
+    <div class="mobile-container">
+      <NavHeader />
+      <main
+        class="text-color bg-primary-light dark:bg-dark-primary-dark dark:text-dark-color min-h-[100vh] py-4">
         <slot />
-      </div>
-    </main>
+      </main>
+    </div>
   </div>
 </template>
 <style>
